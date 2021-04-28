c9810eca6a2759f2b1de9fa2ed68695fddbaf629
sinku <sininoir@gmail.com>
2021-04-29 00:50:18 +0430

    started working on getRandomBlock function

--- block.cpp
+++ block.cpp
@@ -1,4 +1,5 @@
 #include "block.hpp"
+#include <cstdlib>
 
 using namespace std;
 
@@ -13,6 +14,7 @@ Block::Block(): tags(){
 NormalBlock::NormalBlock(bool no_chest){
     tags.push_back("random");
     tags.push_back("loot");
+    tags.push_back("prompt");
     rarity = 1;
     name = "normal";
     if (no_chest){
@@ -23,7 +25,7 @@ NormalBlock::NormalBlock(bool no_chest){
     has_prompt = contains_item;
     has_adjacent_dialog = contains_item;
     if (contains_item){
-        item_inside = get_random_item(0);
+        item_inside = getRandomItem(0);
     }
 }
 
@@ -43,4 +45,38 @@ std::string NormalBlock::getString(){
 
 void NormalBlock::prompt_handler(int ans){
     return;
+}
+
+DigableBlock::DigableBlock(){
+    tags.push_back("random");
+    tags.push_back("loot");
+    rarity = 10;
+    name = "digable";
+    contains_item = _random() < ITEM_CHANCE;
+    has_prompt = false;
+    has_adjacent_dialog = false;
+    if (contains_item){
+        item_inside = getRandomItem(0);
+    }
+}
+
+
+
+
+
+
+
+
+
+Block getRandomBlock(){
+    int block_id = (rand() % NUMBER_OF_BLOCKS) + 1;
+    switch (block_id) {
+        case 1: return NormalBlock();
+        case 2: return DigableBlock();
+        case 3: ;
+        case 4: ;
+        case 5: ;
+        case 6: ;
+    }
+    return Block();
 }
