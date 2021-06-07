abf799c581296f31d91a1fc19d2e2b24960ebe24
sinku <sininoir@gmail.com>
2021-06-07 17:32:27 +0430

    Update getRandomEnemy

--- enemy.cpp
+++ enemy.cpp
@@ -1,5 +1,19 @@
 #include "enemy.hpp"
 
-Enemy* getRandomEnemy(float difficulty_factor){
-    return new Enemy();
+
+Enemy* getEnemy(int enemy_id){
+    switch (enemy_id) {
+        case 1: return new Guy();
+        case 2: return new Wolf();
+        case 3: return new BigBob();
+        case 4: ;
+        case 5: ;
+        case 6: ;
+        default: return new Enemy();
+    }
+}
+
+Enemy* getRandomBlock(){
+    int enemy_id = (rand() % NUMBER_OF_ENEMIES) + 1;
+    return getEnemy(enemy_id);
 }
