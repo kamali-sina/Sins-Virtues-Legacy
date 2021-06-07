f9adcc53e9b5fc407ccc879ad312c9cad41bc512
sinku <sininoir@gmail.com>
2021-06-07 14:57:49 +0430

    Add base Map class and test code for Polymorphism

--- map.hpp
+++ map.hpp
@@ -1,1 +1,20 @@
-//TODO: make map
+#ifndef _MAP_HPP
+#define _MAP_HPP
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include "block.hpp"
+
+class Map{
+    public:
+    Map(){
+        for (int i = 0 ; i < 10 ; i++){
+            Block* block = getRandomBlock();
+            std::cout<<block->getInfo()<<std::endl;
+        }
+    }
+};
+
+
+#endif
