332a5411960ec1840d906b3f4ed9d5dd3191b3b1
sinku <sininoir@gmail.com>
2021-06-09 15:26:33 +0430

    Add spawnRandomBlockAtLocation and useful macros

--- map.hpp
+++ map.hpp
@@ -5,18 +5,19 @@
 #include <vector>
 #include <iostream>
 #include <utility>
+#include <stdlib.h>
 #include "block.hpp"
+#include "utilities.hpp"
 
 #define MAPSIZE 21
+#define BLOCK_ID 0
+#define BLOCK_SPAWN_NUMBER 1
+#define BLOCK_SPAWN_ROW 2
+#define BLOCK_SPAWN_COLUMN 3
 
 class Map{
   public:
-    Map(){
-        for (int i = 0 ; i < 10 ; i++){
-            Block* block = getRandomBlock();
-            std::cout<<block->getInfo()<<std::endl;
-        }
-    }
+    Map();
     void printFullMap();
     void printPartialMap(int vision);
     bool isLocationValid(std::pair<int,int> location);
@@ -27,7 +28,8 @@ class Map{
     std::vector<std::vector<Block *>> map;
     void initMap(); //all map is Block
     void spawnSpecialBlocks(); //Special ones are created
-    void spawnOneSpecialBlock(); //it needs inputs!
+    void spawnOneSpecialBlock(int init_vector[]); //it needs inputs!
+    void spawnRandomBlockAtIndex(std::pair<int,int> index);
     void completeMap(); //Complete rest of the map!
     std::pair<int,int> locationToIndex(std::pair<int,int> location);
     std::pair<int,int> indexToLocation(std::pair<int,int> index);
