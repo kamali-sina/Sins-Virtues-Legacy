a3ea359ef70d743ec8f8a8077265a3bab730d448
sinku <sininoir@gmail.com>
2021-07-04 12:55:59 +0430

    Add base handlers file

---
+++ handlers.hpp
@@ -0,0 +1,14 @@
+#ifndef _HANDLERS_HPP
+#define _HANDLERS_HPP
+
+#include "game.hpp"
+
+void runBlockHandler(bool ans, Game &game);
+void handleNormalBlock(bool ans, Game &game);
+void handleHomeBlock(bool ans, Game &game);
+void handleShopBlock(bool ans, Game &game);
+void handleBlacksmithBlock(bool ans, Game &game);
+
+
+
+#endif
