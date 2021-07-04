b6986f957599c45cb9a244751d032b87fb544ddb
sinku <sininoir@gmail.com>
2021-07-04 12:54:26 +0430

    Add helper functions and clean code

--- block.hpp
+++ block.hpp
@@ -21,11 +21,13 @@ class Block{
     public:
     Block();
     bool tagsContain(std::string tag);
-    virtual std::string getInfo(){return "info_base";}
-    virtual std::string getString(){return colored(name, color);}
-    virtual std::string getPrompt(){return "prompt_base";}
-    virtual std::string getAdjacentDialog(){return "adjacent_base";}
-    virtual void prompt_handler(int ans){}
+    void setHasPrompt(bool value) { has_prompt = value; }
+    void setHasAdjacentDialog(bool value) { has_adjacent_dialog = value; }
+    virtual std::string getInfo() { return "info_base"; }
+    virtual std::string getString() { return colored(name, color); }
+    virtual std::string getPrompt() { return "prompt_base"; }
+    virtual std::string getAdjacentDialog() { return "adjacent_base"; }
+    virtual void prompt_handler(int ans) {  }
 
     std::string name;
     std::vector<std::string> tags;
@@ -37,15 +39,17 @@ class Block{
 
 class NormalBlock : public Block{
     public:
-    float ITEM_CHANCE = 0.04;
+    float ITEM_CHANCE = 1;
     NormalBlock(bool no_chest=false);
 
     void prompt_handler(int ans);
     std::string getInfo();
-    bool getContainsItem(){return contains_item;}
+    bool getContainsItem() { return contains_item; }
+    void setContainsItem(bool value) { contains_item = value; }
     std::string getString();
-    std::string getPrompt(){return "Open the chest?(y,n)";}
-    std::string getAdjacentDialog(){return "I can see a chest over there on the ground!";}
+    std::string getPrompt() { return "Open the chest?(y,n)"; }
+    std::string getAdjacentDialog() { return "I can see a chest over there on the ground!"; }
+    Item* getItemInside(){ return item_inside; }
 
     protected:
     bool contains_item;
@@ -57,9 +61,9 @@ class DigableBlock : public Block{
     float ITEM_CHANCE = 0.85;
     DigableBlock();
 
-    std::string getInfo(){return "It looks like I can dig here with a shovel!";}
-    bool getContainsItem(){return contains_item;}
-    std::string getString(){return name;}
+    std::string getInfo() { return "It looks like I can dig here with a shovel!"; }
+    bool getContainsItem() { return contains_item; }
+    std::string getString() { return name; }
 
     protected:
     bool contains_item;
@@ -73,10 +77,10 @@ class HomeBlock : public Block{
     HomeBlock();
 
     void prompt_handler(int ans);
-    std::string getInfo(){return "This looks like a place to rest.";}
-    bool getContainsItem(){return contains_item;}
-    std::string getPrompt(){return "Enter the home?(y,n)";}
-    std::string getAdjacentDialog(){return "I can see a faint light emitting nearby...";}
+    std::string getInfo() { return "This looks like a place to rest."; }
+    bool getContainsItem() { return contains_item; }
+    std::string getPrompt() { return "Enter the home?(y,n)"; }
+    std::string getAdjacentDialog() { return "I can see a faint light emitting nearby..."; }
 
     protected:
     bool contains_item;
@@ -94,9 +98,9 @@ class ShopBlock : public Block{
     int indexItem(std::string item_name);
     void init_stock();
 
-    std::string getInfo(){return "I can spend the coins I found here and sell my extra stuff.";}
-    std::string getPrompt(){return "Enter the shop?(y,n)";}
-    std::string getAdjacentDialog(){return "I can see a shop nearby...";}
+    std::string getInfo() { return "I can spend the coins I found here and sell my extra stuff."; }
+    std::string getPrompt() { return "Enter the shop?(y,n)"; }
+    std::string getAdjacentDialog() { return "I can see a shop nearby..."; }
 
     protected:
     std::vector<Item> Stock;
@@ -109,9 +113,9 @@ class BlacksmithBlock : public Block{
     //TODO: complete these
     void prompt_handler(int ans);
 
-    std::string getInfo(){return "I can upgrade my weapons and dismantle the extra stuff I found here.";}
-    std::string getPrompt(){return "Enter the blacksmith?(y,n)";}
-    std::string getAdjacentDialog(){return "I can hear hitting on an anvil nearby.";}
+    std::string getInfo() { return "I can upgrade my weapons and dismantle the extra stuff I found here."; }
+    std::string getPrompt() { return "Enter the blacksmith?(y,n)"; }
+    std::string getAdjacentDialog() { return "I can hear hitting on an anvil nearby."; }
 };
 
 Block *getBlock(int block_id);
