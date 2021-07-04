1311310835508ea2b14de0786a21109b43459219
sinku <sininoir@gmail.com>
2021-07-04 12:57:07 +0430

    Add new functions regarding health and location

--- player.hpp
+++ player.hpp
@@ -1,21 +1,27 @@
 #include "item.hpp"
+#include "dialogs.hpp"
 #include <vector>
 #include <string>
 #include <utility>
 
 
-class Player{
+class Player {
     public:
     Player();
     int getDamaged(int damage);
-    int getMaxHp(){return max_hp;}
+    int getMaxHp() { return max_hp; }
+    std::pair<int,int> getLocation();
+    int refillHP();
+    int heal(int amount);
+    void addItem(Item* item);
+
     protected:
-    std::vector<Item> inventory;
+    std::vector<Item*> inventory;
     int max_hp=10;
     int hp;
     int coin=0;
     int scrap=0;
     std::pair<int,int> location;
-    //TODO: set to fizst
-    Item equipped;
+    //TODO: set to fists
+    Item* equipped;
 };
