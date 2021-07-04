769ec0db915fa688749935482af70eed4cc3da0d
sinku <sininoir@gmail.com>
2021-07-04 12:54:39 +0430

    Add new dialogs and clean code

--- dialogs.cpp
+++ dialogs.cpp
@@ -1,6 +1,6 @@
 #include "dialogs.hpp"
 
-void intro_cutscene(){
+void introCutscene() {
     cprint("\n======== Sins & Virtues ========", "blue");
     cprint("=== A game made by PapaSinku ===\n", YELLOW);
     dialog("Unknown", "Have you ever been to the SinkuLand?\n", "red", 8);
@@ -11,62 +11,70 @@ void intro_cutscene(){
     dialog("You", "Where the hell am I? It's getting dark, better find shelter.", YELLOW, 7);
 }
 
-void out_of_bounds_dialog(){
+void outOfBoundsDialog() {
     dialog("You", "Ohoh!!! there is a cliff here! I can't move this direction.", YELLOW, 20);
 }
 
-void dont_have_items_dialog(){
+void dontHaveItemsDialog() {
     dialog("You", "I do not have that item!", YELLOW, 18);
 }
 
-void item_not_in_stock_dialog(){
+void itemNotInStockDialog() {
     dialog("Shopkeeper", "I do not have that item in stock!", "green", 18);
 }
 
-void not_enough_coins_dialog(){
+void notEnoughCoinsDialog() {
     dialog("Shopkeeper", "You do not have enough coins to buy that!", "green", 18);
 }
 
-void welcome_to_shop_dialog(){
+void welcomeToShopDialog() {
     dialog("Shopkeeper", "Welcome stranger! Whatever you want, I got it.", "green", 18);
 }
 
-void cant_use_item_dialog(){
+void cantUseItemDialog() {
     dialog("You", "I can't use that item!", YELLOW, 17);
 }
 
-void cant_dig_here_dialog(){
+void cantDigHereDialog() {
     dialog("You", "This is not a digable block!", YELLOW, 17);
 }
 
-void found_item_dialog(std::string item_name){
+void foundItemDialog(std::string item_name) {
     dialog("You", "I found a " + item_name + "!", YELLOW, 17);
 }
 
-void didnt_find_item_dialog(){
+void didntFindItemDialog() {
     dialog("You", "There was nothing here!", YELLOW, 17);
 }
 
-void new_block_reached_dialog(std::string block_name, std::string block_info){
+void newBlockReachedDialog(std::string block_name, std::string block_info) {
     std::cout<<colored("You",YELLOW) + ": reached a " + block_name + " block. " + block_info;
 }
 
-void death_dialog(){
+void deathDialog() {
     dialog("You", "fuck, is this what death is? god damn it hurts...\nI'm just gonna close my eyes for a little bit...\n\ngoodbye...", YELLOW, 6);
 }
 
-void default_kill_dialog(){
+void defaultKillDialog() {
     dialog("You", "It's over, time to move on...", YELLOW, 17);
 }
 
-void cant_attack_with_item_dialog(){
+void cantAttackWithItemDialog() {
     dialog("You", "I can't attack with that item!", YELLOW, 23);
 }
 
-void into_the_castle_dialog(int number_of_enemies, std::string boss_name){
+void intoTheCastleDialog(int number_of_enemies, std::string boss_name) {
     dialog("You", "Here goes nothing...\nThere are " + std::to_string(number_of_enemies) + " enemies here and a " + boss_name + " boss!", YELLOW, 23);
 }
 
-void boss_dialog(){
+void bossDialog() {
     dialog("You", "Ok time to fight this big bitch!", YELLOW, 23);
+}
+
+void noNormalblockPromptDialog() {
+    dialog("You", "Yeah I'll play it safe!", YELLOW, 23);
+}
+
+void yesNormalblockPromptDialog() {
+    dialog("You", "The chest is empty now.", YELLOW, 23);
 }
