f3110bcf0b1c3a51f012cdc1262965187a538938
sinku <sininoir@gmail.com>
2021-07-07 20:09:14 +0430

    Add new base handlers

--- handlers.cpp
+++ handlers.cpp
@@ -2,8 +2,28 @@
 
 using namespace std;
 
-void handleNormalBlock(bool ans, Game &game){
-    if (ans == false){
+void runBlockHandler(bool ans, Game &game) {
+    Block *block = game.getBlockAtPlayerLocation();
+    switch (block->getID()){
+        case NORMALBLOCK:
+            handleNormalBlock(ans, game);
+            break;
+        case HOMEBLOCK:
+            handleHomeBlock(ans, game);
+            break;
+        case SHOPBLOCK:
+            handleShopBlock(ans, game);
+            break;
+        case BLACKSMITHBLOCK:
+            handleBlacksmithBlock(ans, game);
+            break;
+        default:
+            handleDefault();
+    }
+}
+
+void handleNormalBlock(bool ans, Game &game) {
+    if (ans == false) {
         noNormalblockPromptDialog();
         return;
     }
@@ -13,4 +33,20 @@ void handleNormalBlock(bool ans, Game &game){
     normalblock->setHasAdjacentDialog(false);
     game.getPlayer()->addItem(normalblock->getItemInside());
     yesNormalblockPromptDialog();
+}
+
+void handleHomeBlock(bool ans, Game &game) {
+    cout<<"Complete home block!"<<endl;
+}
+
+void handleShopBlock(bool ans, Game &game) {
+    cout<<"Complete shop block!"<<endl;
+}
+
+void handleBlacksmithBlock(bool ans, Game &game) {
+    cout<<"Complete blacksmith block!"<<endl;
+}
+
+void handleDefault() {
+    _error("There was a problem running the hanndler. error: DEFAULT");
 }
