50fe8a8d318c0b0101503aa49c6185afcbdd0dc1
sinku <sininoir@gmail.com>
2021-07-18 12:19:35 +0430

    Add base compass function

--- map.hpp
+++ map.hpp
@@ -25,6 +25,8 @@ class Map {
     Block* getBlockAtLocation(std::pair<int,int> location);
     void setBlockAtLocation(std::pair<int,int> location, Block* block);
     void printAdjacentDialogs(std::pair<int,int> location);
+    //TODO:
+    void compass(std::pair<int,int> location);
 
   private:
     std::vector<std::vector<Block *>> map;
@@ -37,45 +39,5 @@ class Map {
     std::pair<int,int> locationToIndex(std::pair<int,int> location);
     std::pair<int,int> indexToLocation(std::pair<int,int> index);
 };
-
-/*
-    
-    def compass(self, tup):
-        vector = (self.castle_location[0] - tup[0], self.castle_location[1] - tup[1])
-        if (vector[0] == 0 and vector[1] == 0):
-            return 'This is the castle block!'
-        direction = ''
-        if (vector[0] > 0):
-            direction += 'north'
-        elif (vector[0] < 0):
-            direction += 'south'
-        if (vector[1] > 0):
-            direction += 'east'
-        elif (vector[1] < 0):
-            direction += 'west'
-        return f'The castle is {colored(direction, "cyan")} from here'
-
-    def get_adjacent_blocks(self, tup):
-        x = set()
-        for i in [-1,0,1]:
-            for j in [-1,0,1]:
-                l = (tup[0] + i, tup[1] + j)
-                if (not self.is_location_valid(l)): continue
-                indexes = self.tup_to_index(l)
-                if (not(i == 0 and j == 0)): x.add(type(self.map[indexes[0]][indexes[1]]))
-        return x
-    
-    def get_adjacent_dialogs(self, tup):
-        x = set()
-        for i in [-1,0,1]:
-            for j in [-1,0,1]:
-                l = (tup[0] + i, tup[1] + j)
-                if (not self.is_location_valid(l)): continue
-                indexes = self.tup_to_index(l)
-                block = self.map[indexes[0]][indexes[1]]
-                if (not block.has_adjacent_dialog): continue
-                if (not(i == 0 and j == 0)): x.add(self.map[indexes[0]][indexes[1]].get_adjacent_dialog())
-        return x
-*/
 
 #endif
