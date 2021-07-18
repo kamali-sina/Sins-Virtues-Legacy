24c88c0643aefda0aeb64290cfbfb4d93adf91fe
sinku <sininoir@gmail.com>
2021-07-18 21:55:09 +0430

    Add new TODOs

--- handlers.hpp
+++ handlers.hpp
@@ -10,6 +10,27 @@ void handleShopBlock(bool ans, Game &game);
 void handleBlacksmithBlock(bool ans, Game &game);
 void handleDefault();
 
+/*
+    TODO:
+class Shovel(UtilityItem):
+    def use(self, game, inventory_index):
+        game.dig_here(inventory_index)
 
+class Compass(UtilityItem):
+    def use(self, game, inventory_index):
+        print(game.map.compass(game.player.location))
+
+class Map(UtilityItem):
+    def use(self, game, inventory_index):
+        MAP_VISION = 2
+        game.player.use_utility(inventory_index)
+        game.map.print_partial_map(game.player.location, MAP_VISION)
+
+class Steroid(UtilityItem):
+    def use(self, game, inventory_index):
+        game.player.use_utility(inventory_index)
+        game.player.use_steroid(inventory_index)
+        print(f'max hp is now {colored(game.player.max_hp, "red")}')
+*/
 
 #endif
