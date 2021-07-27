b327c2a22547781c1272c90bc8758b32e91fd4d4
Sinku <sininoir@gmail.com>
2021-07-27 12:25:44 +0430

    Add Item handlers

--- handlers.cpp
+++ handlers.cpp
@@ -39,21 +39,64 @@ void handleNormalBlock(bool ans, Game &game) {
 }
 
 void handleHomeBlock(bool ans, Game &game) {
+    //TODO: 
     cout<<"Complete home block!"<<endl;
 }
 
 void handleShopBlock(bool ans, Game &game) {
+    //TODO: 
     cout<<"Complete shop block!"<<endl;
 }
 
 void handleBlacksmithBlock(bool ans, Game &game) {
+    //TODO: 
     cout<<"Complete blacksmith block!"<<endl;
 }
 
 void handleCastleBlock(bool ans, Game &game) {
+    //TODO: 
     cout<<"Complete castle block!"<<endl;
 }
 
 void handleDefault() {
     _error("There was a problem running the handler. error: DEFAULT");
+}
+
+void runItemHandler(Game &game, int inventory_index) {
+    UtilityItem* item = (UtilityItem*)game.getPlayer()->getItemAtIndex(inventory_index);
+    switch (item->getID()){
+        case SHOVEL:
+            handleShovel(game, inventory_index);
+            break;
+        case COMPASS:
+            handleCompass(game, inventory_index);
+            break;
+        case MAP:
+            handleMap(game, inventory_index);
+            break;
+        case STEROID:
+            handleSteroid(game, inventory_index);
+            break;
+        default:
+            handleDefault();
+    }
+}
+
+void handleShovel(Game &game, int inventory_index) {
+    game.digHere(inventory_index);    
+}
+
+void handleCompass(Game &game, int inventory_index) {
+    cout<<game.getMap().compass(game.getPlayer()->getLocation())<<endl;
+}
+
+void handleMap(Game &game, int inventory_index) {
+    game.getPlayer()->useItem(inventory_index);
+    game.getMap().printPartialMap(game.getPlayer()->getVision(), game.getPlayer()->getLocation());    
+}
+
+void handleSteroid(Game &game, int inventory_index) {
+    game.getPlayer()->useItem(inventory_index);
+    int new_max = game.getPlayer()->useSteroid();
+    usedSteroidDialog(new_max);
 }
