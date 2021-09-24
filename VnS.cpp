1f7ab94ee79ab936b808f36d59f63ece4525fe6b
sinku <sininoir@gmail.com>
2021-09-24 23:56:48 +0330

    Remove extra code

--- VnS.cpp
+++ VnS.cpp
@@ -8,31 +8,11 @@ Game session;
 
 int main (int argc , char *argv[]) {
     srand((unsigned int)time(NULL));
-    if (argc < 2){
-        _error ("no arguments were provided.\n\nif you need help use -h or --help");
-        return 0;
-    }
-    string option = argv[1];
-    string input_path;
-    if (argc > 2) {
-        input_path = argv[2];
-    } else {
-        input_path = DEFAULTSAVEPATH;
-    }
-    bool newgame = false;
+    bool newgame = true;
     string save_path = ""; 
-    toturialIfNeeded(option);
-    helpIfNeeded(option);
-    if (option == NEWGAME) {
-        save_path = handleNewGame(input_path);
-        newgame = true;
-    } else if (option == LOADGAME) {
-        save_path = handleLoadGame(input_path);
-        newgame = false;
-    } else { 
-        _error("Invalid option was provided, use --help for more info!");
-    }
-    session = Game(newgame, save_path);
+    int seed = NOTFOUND;
+    handleArgv(argc, argv, newgame, save_path, seed);
+    session = Game(newgame, save_path, seed);
     session.run();
-    return 1;
+    return 0;
 }
