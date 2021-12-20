602bf76a677dfec54f3e3d6e0a149ed927adab44
Sinku <sininoir@gmail.com>
2021-12-20 21:12:15 +0330

    Add dev_mode support

--- VnS.cpp
+++ VnS.cpp
@@ -6,13 +6,14 @@ using namespace std;
 
 Game session;
 
-int main (int argc , char *argv[]) {
+int main(int argc , char *argv[]) {
     srand((unsigned int)time(NULL));
     bool newgame = true;
+    bool dev_mode = false;
     string save_path = ""; 
     int seed = NOTFOUND;
-    handleArgv(argc, argv, OUTPARAM newgame, OUTPARAM save_path, OUTPARAM seed);
-    session = Game(newgame, save_path, seed);
+    handleArgv(argc, argv, OUTPARAM newgame, OUTPARAM save_path, OUTPARAM seed, OUTPARAM dev_mode);
+    session = Game(newgame, save_path, seed, dev_mode);
     session.run();
     return 0;
 }
