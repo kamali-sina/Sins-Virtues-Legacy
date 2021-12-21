abd12f3e9661a1107ccdb9c9ae61bc0111a58f03
Sinku <sininoir@gmail.com>
2021-12-21 23:43:25 +0330

    Add base dps effects and poison

--- status_effect.cpp
+++ status_effect.cpp
@@ -1,6 +1,10 @@
 #include "status_effect.hpp"
 #include "VnS.hpp"
 
+using namespace std;
+
+/* ==================== Base Status Effect ==================== */
+
 StatusEffect::StatusEffect()  { 
     reset();
 }
@@ -25,4 +29,45 @@ void StatusEffect::apply() {
     _error("base apply was called!");
 }
 
+/* ==================== DPS Status Effect Base ==================== */
 
+DPSEffect::DPSEffect() {
+    INITIAL_TURNS = 0;
+    hp_change = 0;
+    name = "dps";
+    color = WHITE;
+    description = "";
+    applyDialog = "";
+    reset();
+}
+
+void DPSEffect::apply() {
+    if (hp_change > 0) {
+        session.getPlayer()->heal(hp_change);
+    } else {
+        session.getPlayer()->getDamaged(-1 * hp_change);
+    }
+    turns_remaining -= 0;
+}
+
+std::string DPSEffect::getApplyDialog() {
+    string mode = "";
+    if (hp_change > 0) {
+        mode = "heal";
+    } else {
+        mode = "damage";
+    }
+    return this->getString() + " will " + mode + " you for the next " + to_string(turns_remaining) + " turn(s)!";
+}
+
+/* ==================== Poison ==================== */
+
+Poison::Poison() {
+    INITIAL_TURNS = 3;
+    hp_change = -2;
+    name = "poison";
+    color = GREEN;
+    description = "A slow poison that will damage you each turn";
+    applyDialog = "";
+    reset();
+}
--- status_effect.hpp
+++ status_effect.hpp
@@ -1,5 +1,5 @@
-#ifndef _ENEMY_HPP
-#define _ENEMY_HPP
+#ifndef _STATUS_EFFECT_HPP
+#define _STATUS_EFFECT_HPP
 
 #include <vector>
 #include <string>
@@ -12,9 +12,9 @@ class StatusEffect {
     StatusEffect();
     virtual void apply();
     virtual void reset();
-    std::string getApplyDialog();
-    std::string getDescription();
-    std::string getString();
+    virtual std::string getApplyDialog();
+    virtual std::string getDescription();
+    virtual std::string getString();
     int getTurnsRemaining() { return turns_remaining; }
     std::string getName() { return name; }
 
@@ -27,56 +27,19 @@ class StatusEffect {
     int turns_remaining;
 };
 
-/*
-class StatusEffect:
-    def __init__(self):
-        self.initial_turns = 0
-        self.name = "statuseffect"
-        self.color = "magenta"
-        # HP change: Has to be - for damage and + for healing
-        self.hpc = 0
-        self.turns = self.initial_turns
-        self.init()
-        self.reset()
-        if self.hpc > 0:
-            self.color = "green"
-        elif self.hpc < 0:
-            self.color = "red"
+class DPSEffect : public StatusEffect {
+    public:
+    DPSEffect();
+    void apply();
+    std::string getApplyDialog();
 
-    def apply(self, target):
-        if self.hpc >= 0:
-            target.heal(abs(self.hpc))
-        else:
-            target.get_damaged(abs(self.hpc))
-        self.turns -= 1
-        print(self.apply_dialog())
+    protected:
+    int hp_change = 0;
+};
 
-    def apply_dialog(self):
-        if self.hpc < 0:
-            return f"I got damaged for {self.hpc}hp by the {self.name}"
-        elif self.hpc > 0:
-            return f"I got healed for {self.hpc}hp by the {self.name}"
-        else:
-            return f"Still affected by the {self.name}"
-
-    def reset(self):
-        self.turns = self.initial_turns
-
-    def init(self):
-        self.name = "status"
-
-    def description(self):
-        word = ""
-        if self.hpc < 0:
-            word = "damage"
-        elif self.hpc > 0:
-            word = "heal"
-        else:
-            return f"{colored(self.name, self.color)} will affect you for the next {self.turns} turns"
-        return f"{colored(self.name, self.color)} will {word} you for {abs(self.hpc)}hp for the next {self.turns} turns"
-
-    def __str__(self):
-        return colored(self.name, self.color)
-*/
+class Poison : public DPSEffect {
+    public:
+    Poison();
+};
 
 #endif
