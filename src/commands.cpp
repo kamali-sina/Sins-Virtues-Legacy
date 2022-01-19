9447d02f6bc15c2b471fcdf1550968b8afc0ccc7
sinku <sininoir@gmail.com>
2022-01-19 17:19:06 +0330

    Fix bugs

--- src/commands.cpp
+++ src/commands.cpp
@@ -3,9 +3,18 @@
 
 using namespace std;
 
+/* ============= Base Class ============== */
+
 Command::Command() {
     command = "";
     args = vector<string>({"arg1", "arg2"});
     scope.push_back(NORMAL);
     description = "base command that is going to be devrived!";
+    dev_command = false;
 }
+
+void Command::commence(std::vector<std::string> splitted_input) {
+    cout<< "base commence was called!" << endl;
+}
+
+/* ============= Commands ============== */
